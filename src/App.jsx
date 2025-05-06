f130d2567bc4211e0deb4e1639ca85b1878f06dd
Wiam AR <aarikawieam@email.com>
2025-05-06 17:41:01 +0100

    welcome to my first react project <3

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./componants/header";
+import Footer from "./componants/footer";
+import MainContainer from "./componants/maincontainer";
+import './App.css';
+
+
+function App(){
+  return(
+    
+    <>
+      <Header />
+      <MainContainer />
+      <Footer />
+    </>
+   
+
+  );
+}
+export default App ;
---
+++ src/componants/maincontainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MainContainer = () => {
+  return (
+   <main>
+    
+     <h2>main container</h2>
+     <p>
+        ertyuio dfghjk rtyuio dfghjk xcvm rtgyhjk ertyu fgh
+     </p>
+
+   </main>
+  )
+}
+
+export default MainContainer
